45cb5f70d7feba451dee1fafea94062ab73e05c9
lucasAlves999 <151699713+lucasAlves999@users.noreply.github.com>
2024-10-25 15:49:40 -0300

    Update routes_professor.py

--- Projeto-API-main/ProjetoApi/routes/routes_professor.py
+++ Projeto-API-main/ProjetoApi/routes/routes_professor.py
@@ -1,4 +1,20 @@
-# Professores routes
-bp.route('/professores', methods=['GET'])(get_professores)
-bp.route('/professores', methods=['POST'])(create_professor)
-bp.route('/professores/<int:professor_id>', methods=['GET'])(get_professor)
+from flask import Blueprint 
+from controllers.professor_controller import get_professor, create_professor, update_professor , delete_professor
+
+bp = Blueprint('professor', __name__)
+
+@bp.route('/professor', methods=['GET'])
+def get_professor_route():
+    return get_professor()
+
+@bp.route('/professor', methods=['POST'])
+def create_professor_route():
+    return create_professor()
+
+@bp.route('/professor/<int:professor_id>', methods=['PUT'])
+def update_professor_route(professor_id):
+    return update_professor(professor_id)
+
+@bp.route('/professor/<int:professor_id>', methods=['DELETE'])
+def delete_professor_route(professor_id):
+    return delete_professor(professor_id)
