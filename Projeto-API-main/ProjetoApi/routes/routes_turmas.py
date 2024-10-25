cf7d637e797dbb3bf75efb5e26d60ed5e61c0ea6
lucasAlves999 <151699713+lucasAlves999@users.noreply.github.com>
2024-10-25 15:50:01 -0300

    Update routes_turmas.py

--- Projeto-API-main/ProjetoApi/routes/routes_turmas.py
+++ Projeto-API-main/ProjetoApi/routes/routes_turmas.py
@@ -1,4 +1,20 @@
-# Turmas routes
-bp.route('/turmas', methods=['GET'])(get_turmas)
-bp.route('/turmas', methods=['POST'])(create_turma)
-bp.route('/turmas/<int:turma_id>', methods=['GET'])(get_turma)
+from flask import Blueprint 
+from controllers.turma_controller import get_turma, create_turma, update_turma, delete_turma
+
+bp = Blueprint('turma', __name__)
+
+@bp.route('/turmas', methods=['GET'])
+def get_turmas_route():
+    return get_turmas()
+
+@bp.route('/turmas', methods=['POST'])
+def create_turma_route():
+    return create_turma()
+
+@bp.route('/turmas/<int:turma_id>', methods=['PUT'])
+def get_turma_route(turma_id):
+    return update_turma(turma_id)
+
+@bp.route('/turmas/<int:turma_id>', methods=['DELETE'])
+def delete_turma_route(turma_id):
+    return delete_turma(turma_id)
